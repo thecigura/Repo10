5ed9421d18bed32fb6020813cdd16a03e66e74e9
Denan <denan.cigura@gmail.com>
2021-09-23 09:26:11 +0200

    JS Ex09 - .hasOwnProperty() method

---
+++ ex09/src/index.js
@@ -0,0 +1,10 @@
+function myFunction(myObj, checkProp) {
+    // Only change code below  this line
+    if(myObj.hasOwnProperty(checkProp)) {
+        return myObj[checkProp];
+    } else {
+    return "Not Found";
+    }
+    // Only change code above this line
+}
+module.exports = myFunction;
