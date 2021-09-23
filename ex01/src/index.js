7c9b3d492ea49851caa64fb10e1e8fa50c343e7e
Denan <denan.cigura@gmail.com>
2021-09-23 08:51:50 +0200

    JS Ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+var myPet = {
+    species: "Husky",
+    name: "Sara",
+    legs: 4,
+    friends: ["Xyc", "Qwe"]
+}
+
+function myFunction(myObj) {
+    return myObj;
+}
+// Only change code above this line
+console.log(myFunction(myPet)); // Change this line
+module.exports = { myPet, myFunction };
