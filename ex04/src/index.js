f48a6c6412c7d2779062ad2437f046d2fe35d0dc
Denan <denan.cigura@gmail.com>
2021-09-23 09:04:09 +0200

    JS Ex04 - Storing Object Properties

---
+++ ex04/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+var players = {
+    7: "Muhamed Besic",
+    10: "Miralem Pjanic",
+    11: "Edin Dzeko",
+}
+
+function myFunction(myObj) {
+    var playerNumber = [10];
+    var player = players[playerNumber];
+
+    return player;
+}
+// Only change code above this line
+module.exports = { players, myFunction };
