aeab242796d3352df895905866e30d74bc6990ae
Denan <denan.cigura@gmail.com>
2021-09-23 09:09:52 +0200

    JS Ex06 - Add NEW Properties to Object

---
+++ ex06/src/index.js
@@ -0,0 +1,16 @@
+// Create lion object here
+var lion = {
+    "name": "Simba",
+    "legs": 4,
+    "tails": 1
+}
+// End of lion object
+
+function myFunction (propName, propValue) { // Change this line
+//Only change code below this line
+    lion[propName] = propValue;
+    return lion;
+// Only change code above this line
+}
+myFunction("roar", "roar-roar"); // Change this line
+module.exports = myFunction;
